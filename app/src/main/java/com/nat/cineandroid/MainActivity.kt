8fd6bd856a869296e9ef2286393ca8f1934f79b1
Noam - Inclinus <noam@MacBook-Pro-de-Noam.local>
2024-10-28 07:55:03 +0100

    feat(youtube): add video player

--- app/src/main/java/com/nat/cineandroid/MainActivity.kt
+++ app/src/main/java/com/nat/cineandroid/MainActivity.kt
@@ -3,18 +3,31 @@ package com.nat.cineandroid
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
+
 
 class MainActivity : AppCompatActivity() {
+
+    // Add field VideoView
+    private lateinit var youTubePlayerView: YouTubePlayerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        youTubePlayerView = findViewById(R.id.youtube_player_view)
+        lifecycle.addObserver(youTubePlayerView)
+
+        youTubePlayerView.addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
+            override fun onReady(youTubePlayer: YouTubePlayer) {
+                val videoId = "GRe3ZsXAZE4"
+                youTubePlayer.loadVideo(videoId, 0f)
+            }
+        })
+
+
     }
 }
