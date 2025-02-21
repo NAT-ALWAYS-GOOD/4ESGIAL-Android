957f3acf794cc705cbb29ebea9332eec3fedfd55
Anaelle EA <anaelle3364@gmail.com>
2025-02-21 17:30:29 +0100

    feat(data): create methods to fetch and store the unreleased movies

--- app/src/main/java/com/nat/cineandroid/core/api/nat/service/MovieService.kt
+++ app/src/main/java/com/nat/cineandroid/core/api/nat/service/MovieService.kt
@@ -1,4 +1,11 @@
 package com.nat.cineandroid.core.api.nat.service
 
+import com.nat.cineandroid.core.api.nat.dto.movie.MovieResponseDTO
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface MovieService {
+    @GET("movies")
+    suspend fun getMovies(@Query("released") released: Boolean? = null): Response<List<MovieResponseDTO>>
 }
