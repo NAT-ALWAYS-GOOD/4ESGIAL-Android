02638605b9ef06b81ac40fb893a474e15263f72e
Tom <bourlard.tom.94@gmail.com>
2025-02-23 17:30:18 +0100

    feat(profile): handle authentication on profile page

---
+++ app/src/main/java/com/nat/cineandroid/data/user/entity/UserClaims.kt
@@ -0,0 +1,6 @@
+package com.nat.cineandroid.data.user.entity
+
+data class UserClaims(
+    val id: String?,
+    val username: String?
+)
--- app/src/main/java/com/nat/cineandroid/ui/user/profile/ProfileFragment.kt
+++ app/src/main/java/com/nat/cineandroid/ui/user/profile/ProfileFragment.kt
@@ -7,14 +7,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.nat.cineandroid.core.api.JwtTokenProvider
 import com.nat.cineandroid.databinding.FragmentProfileBinding
 import com.nat.cineandroid.ui.MovieDetailFragmentArgs
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 import kotlin.getValue
 
 @AndroidEntryPoint
 class ProfileFragment : Fragment() {
+    @Inject lateinit var jwtTokenProvider: JwtTokenProvider
     private var _binding: FragmentProfileBinding? = null
     private val binding get() = _binding!!
     private val viewModel: ProfileViewModel by activityViewModels()
@@ -30,9 +34,24 @@ class ProfileFragment : Fragment() {
         return binding.root
     }
 
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val navController = findNavController()
+
+        val token = jwtTokenProvider.getToken()
+        if (token.isNullOrEmpty()) {
+            val action = ProfileFragmentDirections.actionProfileFragmentToLoginFragment()
+            navController.navigate(action)
+            return
+        }
+        val userClaims = jwtTokenProvider.getUserClaimsFromJwt(token)
+        if (userClaims?.id.isNullOrEmpty() || userClaims.username.isNullOrEmpty()) {
+            val action = ProfileFragmentDirections.actionProfileFragmentToLoginFragment()
+            navController.navigate(action)
+            return
+        }
+        binding.welcomeMessage.text = "Bienvenue, ${userClaims.username}!"
 
         viewModel.reservations.observe(viewLifecycleOwner) { reservations ->
             Log.d("ProfileFragment", "Reservations: $reservations")
@@ -59,6 +78,12 @@ class ProfileFragment : Fragment() {
             pastAdapter.submitList(reservations)
         }
 
-        viewModel.fetchReservations(1)
+        binding.logoutButton.setOnClickListener {
+            jwtTokenProvider.clearToken()
+            val action = ProfileFragmentDirections.actionProfileFragmentToLoginFragment()
+            navController.navigate(action)
+        }
+
+        viewModel.fetchReservations(userClaims.id.toInt())
     }
 }
